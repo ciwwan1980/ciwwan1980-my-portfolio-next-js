5647e372cd69e21b2524f64de1ee420b74ad4a0e
Ajil Abdalqader <ajilqado@gmail.com>
2020-07-01 03:46:55 +0200

    redirecting-using-history-api-with-next.js

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,14 @@
-const about =()=>{
-return (
-<div> 
+import Link from "next/link"
+
+export default ()=>( 
+
+    <div> 
     <h1>About</h1>
+      <Link href="/">
+        <a >Go to Home</a>
+    </Link>
     <p>JavaScript programmer</p>
     <img src="/static/javascript-logo.png" alt=""/>
     </div>
-)
 
-}
-export default about;
+)
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 const index = () => {
     return ( 
     <div>
     <h1>Home</h1>
-    Welocme to Page
+    <Link href="/about">
+    <a >Go to about</a>
+    </Link>
+    
+    <p>Welocme to the Home Page</p>
     </div>
     )
 }
