6ff78c26f69fcd35ee5be41ce957eaf8f7a4746a
Ajil Abdalqader <ajilqado@gmail.com>
2020-07-11 01:31:19 +0200

    fetching-date-github-render

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,26 @@
 import Link from "next/link"
 import Layout from "../components/Layout"
+import React, { Component } from 'react'
 
-export default ()=>( 
+export default class about extends Component {
+state={
+    user:null
+}
+    componentDidMount(){
+        fetch("https://api.github.com/users/ciwwan1980")
+        .then(res=>res.json())
+        .then(data=>{
+            this.setState({
+                user:data
+            })
+        })
+    }
+    render() {
+        return (
+            <div>
+      
     <Layout title="About"> 
-  
+    {JSON.stringify(this.state.user)}
     <Link href="/">
         <a >Go to Home</a>
     </Link>
@@ -11,5 +28,12 @@ export default ()=>(
     <p>JavaScript programmer</p>
     <img src="/static/javascript-logo.png" alt=""/>
     </Layout>
+ 
+            </div>
+        )
+    }
+}
 
-)
+
+
+
