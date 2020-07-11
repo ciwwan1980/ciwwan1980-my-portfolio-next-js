860d4a4ad8bfc64f80e870ceb89836e11e79a92c
Ajil Abdalqader <ajilqado@gmail.com>
2020-07-11 03:21:30 +0200

    fetch-with-getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,33 +1,47 @@
 import Link from "next/link"
 import Layout from "../components/Layout"
+import fetch from "isomorphic-unfetch";
 import React, { Component } from 'react'
 
 export default class about extends Component {
-state={
-    user:null
-}
-    componentDidMount(){
-        fetch("https://api.github.com/users/ciwwan1980")
-        .then(res=>res.json())
-        .then(data=>{
-            this.setState({
-                user:data
-            })
-        })
-    }
+
+ static async getInitialProps() {
+    const res = await fetch("https://api.github.com/users/ciwwan1980");
+    const data = await res.json();
+
+    return { user: data };
+  }
+// static getInitialProps() {
+// fetch("https://api.github.com/users/ciwwan1980")
+//   .then(res=>res.json())
+//   .then(data=>{
+//       console.log(data)
+//   })
+
+//     return { };
+//   }
+// state={
+//     user:null
+// }
+
+    // componentDidMount(){
+    //     fetch("https://api.github.com/users/ciwwan1980")
+    //     .then(res=>res.json())
+    //     .then(data=>{
+    //         this.setState({
+    //             user:data
+    //         })
+    //     })
+    // }
     render() {
+    const { user} = this.props;
         return (
             <div>
       
-    <Layout title="About"> 
-    {JSON.stringify(this.state.user)}
-    <Link href="/">
-        <a >Go to Home</a>
-    </Link>
-    
-    <p>JavaScript programmer</p>
-    <img src="/static/javascript-logo.png" alt=""/>
-    </Layout>
+     <Layout title="About">
+        <p>{user.name}</p>
+        <img src={user.avatar_url} alt="Reed" height="200px" />
+      </Layout>
  
             </div>
         )
