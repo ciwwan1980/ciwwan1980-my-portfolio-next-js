5a68273c8950b962dfa2b932cdd84671b7cde04c
Ajil Abdalqader <ajilqado@gmail.com>
2020-07-14 23:41:23 +0200

    using-slugprops-in-url

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,9 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 
-const PostLink = ({title }) => (
+const PostLink = ({ slug, title }) => (
   <li>
-    <Link href={`/post?title=${title}`}>
+    <Link as={`/${slug}`} href={`/post?title=${title}`}>
       <a>{title}</a>
     </Link>
   </li>
@@ -12,10 +12,9 @@ const PostLink = ({title }) => (
 export default () => (
   <Layout title="My Blog">
     <ul>
-      <PostLink  title="React Post" />
-      <PostLink  title="Angular Post" />
-      <PostLink  title="Vue Post" />
+      <PostLink slug="react-post" title="React Post" />
+      <PostLink slug="angular-post" title="Angular Post" />
+      <PostLink slug="vue-post" title="Vue Post" />
     </ul>
   </Layout>
 );
-
