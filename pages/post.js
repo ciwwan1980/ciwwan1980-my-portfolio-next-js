614f8c525546dd3d075f734f00cf5e1523dee31b
Ajil Abdalqader <ajilqado@gmail.com>
2020-07-15 00:44:33 +0200

    withrouter-as-props

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,14 @@
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import { withRouter } from "next/router";
 
- const post=({url})=>{
-     console.log(url.query.title,"URL")
-    return ( 
-     <Layout title={url.query.title}>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero soluta explicabo voluptate quis repudiandae quibusdam reprehenderit earum? Expedita minima eveniet modi id alias hic, fugit corporis ut dolore, aliquid est.</p>
+const Post = ({ router }) => (
+  <Layout title={router.query.title}>
+    <p style={{ width: "80vw" }}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias,
+      eaque! Ad explicabo eum cumque, voluptate quaerat perferendis eligendi
+      eveniet sint dolorem in odit sed mollitia, eos rerum, quod saepe atque!
+    </p>
   </Layout>
-  )
-}
-export default post
+);
+
+export default withRouter(Post);
